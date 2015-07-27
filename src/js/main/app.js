b561a743673b64c9bb03ff1c64e5230c20d5c176
Kelum Senanayake <kelumkps@yahoo.com>
2015-07-27 12:01:24 +0800

    Fixing SideBar not displaying issue when clicking Menu

--- src/js/main/app.js
+++ src/js/main/app.js
@@ -15,36 +15,42 @@ app.config(['$routeProvider', '$locationProvider', '$httpProvider', function ($r
         {
             templateUrl: 'home.html',
             controller: 'MainController',
-            access: access.user
+            access: access.user,
+            reloadOnSearch: false
         });
     $routeProvider.when('/login',
         {
             templateUrl: 'login.html',
             controller: 'LoginCtrl',
-            access: access.guest
+            access: access.guest,
+            reloadOnSearch: false
         });
     $routeProvider.when('/register',
         {
             templateUrl: 'register.html',
             controller: 'RegisterCtrl',
-            access: access.guest
+            access: access.guest,
+            reloadOnSearch: false
         });
     $routeProvider.when('/settings',
         {
             templateUrl: 'settings.html',
             controller: 'RegisterCtrl',
-            access: access.public
+            access: access.public,
+            reloadOnSearch: false
         });
     $routeProvider.when('/private',
         {
             templateUrl: 'private.html',
             controller: 'MainController',
-            access: access.user
+            access: access.user,
+            reloadOnSearch: false
         });
     $routeProvider.when('/404',
         {
             templateUrl: '404.html',
-            access: access.public
+            access: access.public,
+            reloadOnSearch: false
         });
     $routeProvider.otherwise({redirectTo: '/404'});
 
