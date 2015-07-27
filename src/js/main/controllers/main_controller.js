31abe4ef7abf043ae57cb69ba8059899d3708c48
Kelum Senanayake <kelumkps@yahoo.com>
2015-07-28 07:36:42 +0800

    Formatting source files

--- src/js/main/controllers/main_controller.js
+++ src/js/main/controllers/main_controller.js
@@ -3,12 +3,12 @@
 angular.module('SGTravelBuddy')
 
     .controller('MainCtrl', ['$scope', function ($scope) {
-    		var index = 500;
-    		$scope.scrollItems = ['4', '5', '6', '28', '59', '19', '81', '101', '120', '150', '293', '489'];
-    		$scope.bottomReached = function () {
-    			if (index < 800) {
-    				index = index + 10;
-    				$scope.scrollItems.push(index + '');
-    			}
-    		}
+        var index = 500;
+        $scope.scrollItems = ['4', '5', '6', '28', '59', '19', '81', '101', '120', '150', '293', '489'];
+        $scope.bottomReached = function () {
+            if (index < 800) {
+                index = index + 10;
+                $scope.scrollItems.push(index + '');
+            }
+        }
     }]);
