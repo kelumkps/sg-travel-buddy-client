f11742ea2864805949cf0ecab9e4cc00e5472e9f
Kelum Senanayake <kelumkps@yahoo.com>
2015-08-04 18:37:33 +0800

    Added http based route notifier service

--- src/js/travel/controllers/bus-controller.js
+++ src/js/travel/controllers/bus-controller.js
@@ -1,8 +1,8 @@
 'use strict';
 
 angular.module('SGTravelBuddy.travel')
-    .controller('BusCtrl', ['$scope', '$translate', '$routeParams', 'BusService', 'Authorizer',
-        function ($scope, $translate, $routeParams, BusService, Authorizer) {
+    .controller('BusCtrl', ['$rootScope','$scope', '$translate', '$routeParams', 'BusService', 'Authorizer', 'NotifierHttpService',
+        function ($rootScope, $scope, $translate, $routeParams, BusService, Authorizer, NotifierHttpService) {
             $scope.messages = {};
             $scope.showTabs = false;
             $scope.showSecondTab = false;
@@ -18,23 +18,78 @@ angular.module('SGTravelBuddy.travel')
             }, function (err) {
                 $scope.messages.error = $translate.instant('views.app.error.service.unavailable');
             });
-            $scope.notifications = [];
-            $scope.notifyStop = function (stop) {
+            $scope.selectedBusStops = [];
+            $scope.busStopsToBeNotified = {};
+            $scope.changeSelectedBusStops = function (stop) {
                 var showIcon = true;
-                if ($scope.notifications.length > 0) {
-                    var index = $scope.notifications.indexOf(stop.number);
+                if ($scope.selectedBusStops.length > 0) {
+                    var index = $scope.selectedBusStops.indexOf(stop.number);
                     if (index > -1) {
-                        $scope.notifications.splice(index, 1);
+                        $scope.selectedBusStops.splice(index, 1);
                         showIcon = false;
+                        var storedStop = $scope.busStopsToBeNotified[stop.number];
+                        storedStop['notify'] = showIcon;
+                        $scope.busStopsToBeNotified[stop.number] = undefined;
                     } else if (Authorizer.isLoggedIn()) {
-                        $scope.notifications.push(stop.number);
+                        $scope.selectedBusStops.push(stop.number);
+                        $scope.busStopsToBeNotified[stop.number] = stop;
                     } else {
                         $scope.messages.warning = $translate.instant('views.bus.multiple.stops.warning');
                         showIcon = false;
                     }
                 } else {
-                    $scope.notifications.push(stop.number);
+                    $scope.selectedBusStops.push(stop.number);
+                    $scope.busStopsToBeNotified[stop.number] = stop;
                 }
                 stop['notify'] = showIcon;
-            }
+
+                NotifierHttpService.updateSelectedBusStops($scope.selectedBusStops);
+                $rootScope.$broadcast('notifier:selectedBusStops', { selectedStops: $scope.selectedBusStops });
+            };
+
+            $scope.disableNotifyButton = false;
+
+            $scope.startNotifier = function () {
+                if ($scope.selectedBusStops.length > 0) {
+                    $scope.disableNotifyButton = true;
+                    NotifierHttpService.createNotifier(function(route) {
+                        console.log('created route response', route);                        
+                        NotifierHttpService.startNotifier(route._id, function(err) {
+                            console.log('notifire start error', err);                           
+                        });
+                    }, function(err) {
+                        console.log('notifire create error', err);
+                        $scope.messages.error = $translate.instant('views.app.error.service.unavailable');
+                        $scope.disableNotifyButton = false;
+                    });
+                }
+            };
+
+            $scope.$on('notifier:busStops', function(event, args) {
+                console.log('inside bus controller on notifier:busStops');
+                var nearBusStops = args.nearStops;
+                nearBusStops.forEach(function (nearStop) {
+                    var storedStop = $scope.busStopsToBeNotified[nearStop];
+                    storedStop['notify'] = false;
+                    var index = $scope.selectedBusStops.indexOf(nearStop);
+                    if (index > -1) {
+                        $scope.selectedBusStops.splice(index, 1);
+                        console.log('splice from selectedBusStops');
+                    }
+                });
+
+
+                //todo remove below
+                if(nearBusStops.length > 1) {
+                    $scope.messages.info = "Following bus stops are colse to your current location \n" + JSON.stringify(nearBusStops);
+                } else {
+                    $scope.messages.info = "Following bus stop is colse to your current location \n" + nearBusStops;
+                }
+
+                
+            });
+
+            $scope.$on('notifier:stopNotifier', function(event) {
+                $scope.disableNotifyButton = false;
+            });
         }]);
---
+++ src/js/travel/services/notifier-http-service.js
@@ -0,0 +1,86 @@
+'use strict';
+
+angular.module('SGTravelBuddy.travel')
+    .service('NotifierHttpService', ['$rootScope', '$http', '$interval', 'getCurrentPosition', 'Authorizer',
+     function ($rootScope, $http, $interval, getCurrentPosition, Authorizer) {
+    	var selectedBusStops = [];
+    	var hasUpdates = false;
+    	var interval;
+
+    	this.createNotifier = function(success, error) {
+			if (selectedBusStops.length > 0) {
+    			getCurrentPosition(function(position) {
+    				console.log(position);
+    				var lat = position.coords.latitude; 
+      				var lng = position.coords.longitude;
+      				var coordinates = [lng, lat];
+      				var routeData = {
+            			coordinates: coordinates
+        			};
+        			if (hasUpdates) routeData['busStops'] = selectedBusStops;
+					$http.post('/api/routes', routeData).success(success).error(error);        			
+  				});
+    		}
+    	};
+
+    	this.startNotifier = function (routeId, error) {
+    		if (selectedBusStops.length > 0) {
+				interval = $interval(function() {
+				  	getCurrentPosition(function(position) {
+	    				console.log('geo position>>>>>>', position);
+	    				var lat = position.coords.latitude; 
+	      				var lng = position.coords.longitude;
+	      				var coordinates = [lng, lat];
+	      				var routeData = {
+	            			coordinates: coordinates
+	        			};
+	        			if (hasUpdates) {
+	        				routeData['busStops'] = selectedBusStops;
+	        				hasUpdates = false;
+	        			}
+	        			updateRouteData(routeId, routeData, error);
+	  				});
+				}, 9000);
+    		}
+    	};
+
+    	this.stopNotifier = function() {
+    		if (interval) {
+    			$interval.cancel(interval);
+    			$rootScope.$broadcast('notifier:stopNotifier');
+    		}
+    	};
+
+		this.updateSelectedBusStops = function(list) {
+			if (list.length > 0) {
+				selectedBusStops = list;
+				hasUpdates = true;
+			} else {
+				hasUpdates = false;
+				this.stopNotifier();
+			}
+    	};
+
+    	function updateRouteData(routeId, routeData, error) {
+			$http.put('/api/routes/' + routeId, routeData).success(function (nearBusStops) {
+				console.log('updated route nearBusStops >>>', nearBusStops);
+				var matchedNearBusStops = [];
+				var isArray = angular.isArray(nearBusStops);
+				selectedBusStops.forEach(function (stop) {
+					if (isArray) {
+						nearBusStops.forEach(function (nearStop) {
+							if (nearStop._id === stop) matchedNearBusStops.push(stop);
+						});
+					} else if (nearBusStops._id === stop) {
+						matchedNearBusStops.push(stop);
+					}				        
+			    });
+
+			    console.log('matches>>>>>>>>>>>>>', matchedNearBusStops);
+		        if (matchedNearBusStops.length > 0) {
+		        	$rootScope.$broadcast('notifier:busStops', { nearStops: matchedNearBusStops });
+		        }				
+			}).error(error);
+    	};
+        
+    }]);
